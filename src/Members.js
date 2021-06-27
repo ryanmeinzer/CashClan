7ea3b1d55eaa7f6cb82bbff6cb0a95d4ef8f7e20
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-06-26 17:42:36 -0700

    deriving seller names for frontend

--- src/Members.js
+++ src/Members.js
@@ -5,7 +5,7 @@ const Members = (props) => {
             <h1>Members:</h1>
             <ul>
                 {props.members.map(member =>
-                    <li>{member.name}</li>
+                    <li key={member.id}>{member.name}</li>
                 )}
             </ul>
         </>
--- src/Transactions.js
+++ src/Transactions.js
@@ -1,13 +1,22 @@
 const Transactions = (props) => {
 
-    console.log(props.transactions)
+    // const getTransactionSellerName = (id) => {
+    //     fetch(`http://localhost:3000/transactions/${id}`)
+    //         .then(response => response.text())
+    //         // .then(text => console.log(text))
+    //         .then(text => text)
+    //         // .then((obj) => obj.json())
+    //         // .then(json => JSON.stringify(json))
+    //         // .then(json => console.log(JSON.stringify(json)))
+    //         // .then(json => json)
+    // }
 
     return (
         <>
             <h1>Transactions:</h1>
             <ul>
                 {props.transactions.map(transaction =>
-                    <li>${transaction.amount} (${(4.44 - 1 - (transaction.amount * .03)).toFixed(2)} saved / ${((1 + transaction.amount * .03)).toFixed(2)} earned)</li>
+                    <li key={transaction.id}>Seller: {props.sellerName(transaction.id)} ${transaction.amount} (${(4.44 - 1 - (transaction.amount * .03)).toFixed(2)} saved / ${((1 + transaction.amount * .03)).toFixed(2)} earned)</li>
                 )}
             </ul>
         </>
