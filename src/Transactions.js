dcb944877175444523aaae0c993f7224eaa1cae4
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-06-27 16:55:42 -0700

    cleaned up Transactions and left commented code in for now

--- src/Transactions.js
+++ src/Transactions.js
@@ -1,15 +1,32 @@
-import React, {useState, useEffect} from "react"
+// import React, {useState, useEffect} from "react"
 
 const Transactions = (props) => {
 
-    const [name, setName] = useState('')
+    // const [name, setName] = useState('')
 
-    useEffect(() => {
-        fetch(`http://localhost:3000/transactions/${props.transactions[0].id}`)
-            .then(response => response.text())
-            // .then(text => console.log(text))
-            .then(text => setName(text))
-    }, [])
+    // const getSellerName = (id) => {
+        
+    //     fetch(`http://localhost:3000/transactions/${id}`)
+    //         .then(response => response.text())
+    //         // .then(text => console.log(text))
+    //         .then(text => setName(text))
+    // }
+
+    // console.log(name)
+
+    // useEffect(() => {
+    //     fetch(`http://localhost:3000/transactions/55`)
+    //         .then(response => response.text())
+    //         .then(text => console.log(text))
+    //         // .then(text => setName(text))
+    // }, [])
+
+    // const transactionsWithSellerNames = 
+    //     <ul>
+    //         {props.transactions.map(transaction =>
+    //             <li key={transaction.id}>${transaction.amount} (${(4.44 - 1 - (transaction.amount * .03)).toFixed(2)} saved / ${((1 + transaction.amount * .03)).toFixed(2)} earned)</li>
+    //         )}
+    //     </ul>
 
     return (
         <>
@@ -19,7 +36,6 @@ const Transactions = (props) => {
                     <li key={transaction.id}>${transaction.amount} (${(4.44 - 1 - (transaction.amount * .03)).toFixed(2)} saved / ${((1 + transaction.amount * .03)).toFixed(2)} earned)</li>
                 )}
             </ul>
-            {/* <p>{name}</p> */}
         </>
     )
 }
