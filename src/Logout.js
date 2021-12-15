950ff2eb02b0ef160b284a291628aff47454edbc
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-12-15 00:12:26 -0800

    deleted unecessary files and updated logout logic

--- src/Logout.js
+++ src/Logout.js
@@ -1,12 +1,19 @@
 import {GoogleLogout} from 'react-google-login'
+import {useMemberContext} from './providers/member'
 
 function Logout() {
 
+    const {isLoggedIn} = useMemberContext()
+    const {setIsLoggedIn} = useMemberContext()
+
     const responseGoogle = (res) => {
         console.log(res)
+        setIsLoggedIn(false)
+        // window.refresh()
     }
 
     return (
+        isLoggedIn &&
         <GoogleLogout
             clientId="495182513894-qpo5gbo9ppe0gucfq6oq0vrkr4mmlpvb.apps.googleusercontent.com"
             buttonText="Logout"
