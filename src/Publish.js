f8c4effadf3f95451d2f0f1e2c5b3c03cc15e110
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-12-29 00:02:15 -0800

    removed excess line

--- src/Publish.js
+++ src/Publish.js
@@ -178,7 +178,6 @@ const Publish = () => {
                 </select>
                 <br />
                 <br />
-                <br />
                 {
                     state.active
                         ?
