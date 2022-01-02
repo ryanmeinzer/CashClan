3b94552b0b12a942d8d7668977e803fa1a6f952d
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-01-02 15:20:36 -0800

    resolving failed TypeError

--- src/Publish.js
+++ src/Publish.js
@@ -18,7 +18,7 @@ const Publish = () => {
             fetch(`https://cashclan-backend.herokuapp.com/members/${member.googleId}`)
                 .then((obj) => obj.json())
                 .then(json => setState(
-                    json.active
+                    json.active === true
                         ?
                         {active: json.active, mode: json.mode, amount: json.amount, premium: json.premium, location: json.location}
                         :
