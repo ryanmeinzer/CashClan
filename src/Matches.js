a12322487841573414faca4dd22c758602860d94
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-01-01 18:42:41 -0800

    sorted matches based on premium

--- src/Matches.js
+++ src/Matches.js
@@ -12,6 +12,9 @@ const Matches = ({offer}) => {
 
     const Matches = () => {
         const matches = members.filter(member => offer.mode === 'buying' ? member.mode === 'selling' && member.amount >= offer.amount && member.premium <= offer.premium && member.location === offer.location : member.mode === 'buying' && member.amount <= offer.amount && member.premium >= offer.premium && member.location === offer.location)
+
+        const sortedMatches = matches.sort(function (a, b) {return a.premium - b.premium})
+
         return (
             <>
                 {matches.length > 0
@@ -19,7 +22,7 @@ const Matches = ({offer}) => {
                     : <h3 style={{color: "red"}}>Your offer has no current matches in the CashClan.</h3>
                 }
                 <ul>
-                    {matches.map(member =>
+                    {sortedMatches.map(member =>
                         // <li key={member.id}>{member.name} {member.mode === 'buying' && 'is buying at least'} {member.mode === 'selling' && 'is selling up to'} {member.amount !== 0 && `$${member.amount}`} {member.location && `at ${member.location}`}</li>
                         <li key={member.id}>{member.name} {member.mode === 'buying' && 'will buy at least'} {member.mode === 'selling' && 'will sell up to'} {member.amount !== 0 && member.amount !== null && `$${member.amount}`} {member.mode === 'buying' && 'and will pay up to a '} {member.mode === 'selling' && 'and must earn at least a '} {member.premium !== 0 && member.premium !== null && `${member.premium}%`} {member.mode === 'buying' && 'cost'} {member.mode === 'selling' && 'profit'} {member.location && `at ${member.location}`}</li>
                     )}
