997358c5554818fe3b20b19dab6b5f6b52394832
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-01-01 18:49:11 -0800

    updated match sort for optimum result of both buying and selling

--- src/Matches.js
+++ src/Matches.js
@@ -13,7 +13,13 @@ const Matches = ({offer}) => {
     const Matches = () => {
         const matches = members.filter(member => offer.mode === 'buying' ? member.mode === 'selling' && member.amount >= offer.amount && member.premium <= offer.premium && member.location === offer.location : member.mode === 'buying' && member.amount <= offer.amount && member.premium >= offer.premium && member.location === offer.location)
 
-        const sortedMatches = matches.sort(function (a, b) {return a.premium - b.premium})
+        function sortedMatches() {
+            if (offer.mode === 'buying') {
+                return matches.sort(function (a, b) {return a.premium - b.premium})
+            } else if (offer.mode === 'selling') {
+                return matches.sort(function (a, b) {return b.premium - a.premium})
+            }
+        }
 
         return (
             <>
@@ -22,7 +28,7 @@ const Matches = ({offer}) => {
                     : <h3 style={{color: "red"}}>Your offer has no current matches in the CashClan.</h3>
                 }
                 <ul>
-                    {sortedMatches.map(member =>
+                    {sortedMatches().map(member =>
                         // <li key={member.id}>{member.name} {member.mode === 'buying' && 'is buying at least'} {member.mode === 'selling' && 'is selling up to'} {member.amount !== 0 && `$${member.amount}`} {member.location && `at ${member.location}`}</li>
                         <li key={member.id}>{member.name} {member.mode === 'buying' && 'will buy at least'} {member.mode === 'selling' && 'will sell up to'} {member.amount !== 0 && member.amount !== null && `$${member.amount}`} {member.mode === 'buying' && 'and will pay up to a '} {member.mode === 'selling' && 'and must earn at least a '} {member.premium !== 0 && member.premium !== null && `${member.premium}%`} {member.mode === 'buying' && 'cost'} {member.mode === 'selling' && 'profit'} {member.location && `at ${member.location}`}</li>
                     )}
