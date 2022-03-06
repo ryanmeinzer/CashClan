65c394cf17c61058537cdb86bf447b8652e9259c
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-03-06 11:59:31 -0800

    moving page visibility refresh logic to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React, {useLayoutEffect} from 'react'
 import Profile from './Profile'
 import EditProfile from './EditProfile'
 import Home from './Home'
 import {Routes, Route} from "react-router-dom"
 
 const App = () => {
+
+  // hard refresh if member has left app/page and returns
+  const onVisibilityChange = () => {
+    if (document.visibilityState === 'visible') {
+      window.location.reload(true)
+    }
+  }
+  useLayoutEffect(() => {
+    document.addEventListener("visibilitychange", onVisibilityChange)
+    return () => document.removeEventListener("visibilitychange", onVisibilityChange)
+  }, [])
 
   return (
     <>
