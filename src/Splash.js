f4918e893bd0587697e46d85dfef9b9b0b28ad41
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-03-18 23:55:35 -0700

    adjusting margins

--- src/Splash.js
+++ src/Splash.js
@@ -25,7 +25,7 @@ const Splash = (props) => {
             <Container>
                 <Hero refresh={props.refresh} />
             </Container >
-            <Box maxWidth={'100%'} bgcolor={'primary.dark'} minHeight={'10rem'} sx={{display: 'flex', alignItems: 'center', mt: -5}} >
+            <Box maxWidth={'100%'} bgcolor={'primary.dark'} minHeight={'10rem'} sx={{display: 'flex', alignItems: 'center', mt: -3}} >
                 <Container >
                     <Partners />
                 </Container>
