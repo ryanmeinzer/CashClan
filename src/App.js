26514a677b7ccfb2e1942ab4debd9a7e3cddd7c9
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-05-24 20:59:03 -0700

    testing alternative select

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from './Home'
 import Privacy from './Privacy'
+import BasicSelect from './BasicSelect'
 import {Routes, Route} from "react-router-dom"
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="privacy" element={<Privacy />} />
+        <Route path="basicselect" element={<BasicSelect />} />
       </Routes>
     </>
   )
