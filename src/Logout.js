c93b89dfffe9cd7c83248040586a398b9be25624
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-05-25 20:56:41 -0700

    resolving logout drop-down flicker on Safari

--- src/Logout.js
+++ src/Logout.js
@@ -1,12 +1,19 @@
 import {GoogleLogout} from 'react-google-login'
 import {useMemberContext} from './providers/member'
 import MenuItem from '@mui/material/MenuItem'
+import {useTheme} from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
 function Logout(props) {
 
     const {isLoggedIn} = useMemberContext()
     const {setIsLoggedIn} = useMemberContext()
     const {setMember} = useMemberContext()
+
+    const theme = useTheme()
+    const isMd = useMediaQuery(theme.breakpoints.up('md'), {
+        defaultMatches: true,
+    })
 
     const responseGoogle = (res) => {
         console.log(res)
@@ -22,10 +29,25 @@ function Logout(props) {
             // ! swap above with below when developing locally
             clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
             render={renderProps => (
-                <MenuItem
-                    onClick={renderProps.onClick}
-                    disabled={renderProps.disabled}
-                >Log Out</MenuItem>
+                isMd
+                    ?
+                    <MenuItem
+                        onClick={renderProps.onClick}
+                        disabled={renderProps.disabled}
+                    >Log Out</MenuItem>
+                    :
+                    <option
+                        style={{
+                            padding: '10px',
+                            fontWeight: 400,
+                            fontSize: '1rem',
+                            lineHeight: 1.5,
+                            letterSpacing: '0.00938em'
+                        }}
+                        onClick={renderProps.onClick}
+                        disabled={renderProps.disabled}>
+                        Log Out
+                    </option>  
             )}
             onLogoutSuccess={responseGoogle}
             onFailure={responseGoogle}
